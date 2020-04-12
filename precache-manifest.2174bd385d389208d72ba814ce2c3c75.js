0e75568f38c89dec204d72a4c5de70c16e5f7e5b
Alistair Jones <alistair@ohalo.co>
2020-04-12 09:48:05 +0100

    Updates

---
+++ precache-manifest.2174bd385d389208d72ba814ce2c3c75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc68ac3ee693a57adc592f7df71eaa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9852f28b5d36cc7c3afe",
+    "url": "/static/css/main.c8ea628f.chunk.css"
+  },
+  {
+    "revision": "29f5e5ac8b2c8797dfdd",
+    "url": "/static/js/2.e40de753.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e40de753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9852f28b5d36cc7c3afe",
+    "url": "/static/js/main.4e6375a5.chunk.js"
+  },
+  {
+    "revision": "290798e2bbc47c9b6ac3",
+    "url": "/static/js/runtime-main.72d5b0aa.js"
+  }
+]);
