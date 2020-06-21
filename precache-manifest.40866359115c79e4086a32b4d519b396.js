0a02c3d314d79fa0fdb0ad62b2e1ce20020cd90c
Alistair Jones <alistair@ohalo.co>
2020-06-22 00:09:00 +0100

    Updates

---
+++ precache-manifest.40866359115c79e4086a32b4d519b396.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6649b44ddc821d84587aaf49f92e8b",
+    "url": "/efferalgang-site/index.html"
+  },
+  {
+    "revision": "9616204d584dcf270018",
+    "url": "/efferalgang-site/static/css/main.c5644fa2.chunk.css"
+  },
+  {
+    "revision": "78131b3f28089c2fdabd",
+    "url": "/efferalgang-site/static/js/2.63587cb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/efferalgang-site/static/js/2.63587cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9616204d584dcf270018",
+    "url": "/efferalgang-site/static/js/main.806050ad.chunk.js"
+  },
+  {
+    "revision": "c2b0e4ea16e8a4bd323d",
+    "url": "/efferalgang-site/static/js/runtime-main.315567ff.js"
+  }
+]);
