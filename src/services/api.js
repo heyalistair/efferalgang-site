4a641137fc02bec61154e85422a2354b81824760
Alistair Jones <alistair@ohalo.co>
2020-07-10 00:19:02 +0100

    Wrong API URL... I really should fix that

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 class Api {
 
-    // API_URL = "https://alistairj.com";
-    API_URL = "http://localhost:8080";
+    API_URL = "https://alistairj.com";
+    // API_URL = "http://localhost:8080";
 
     getPlayerInfo() {
 
